c8e20a6a1b31fa8069b80714bebbbe29b07b5b56
Drew Conly <atconly@gmail.com>
2016-07-28 21:16:03 -0600

    cors

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails_12factor', group: :production
 
 gem 'rails', '4.2.3'
+gem 'rails-api'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+  end
+
   def home
     render 'layouts/application'
   end
